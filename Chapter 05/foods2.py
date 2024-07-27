3ba8451f404691ac0893ad59f5acf1f517af27b1
Kravlan <121265451+XVItokaz@users.noreply.github.com>
2024-07-28 00:14:13 +0200

    Create foods2.py
    New file incomming

---
+++ Chapter 05/foods2.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friends_food = my_foods[:]
+
+my_foods.append('Oxfile')
+friends_food.append('Falukorv')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friends faviorite foods are:")
+print(friends_food)
